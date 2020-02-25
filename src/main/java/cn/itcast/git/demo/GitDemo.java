3d8024bc198396f43cbc5000c621dc3ce84b55d2
a1132442002 <61455468+a1132442002@users.noreply.github.com>
2020-02-25 23:15:40 +0800

    Update GitDemo.java

--- src/main/java/cn/itcast/git/demo/GitDemo.java
+++ src/main/java/cn/itcast/git/demo/GitDemo.java
@@ -5,5 +5,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("测试");
         System.out.println("更新");
+        System.out.println("添加档案");
     }
 }
